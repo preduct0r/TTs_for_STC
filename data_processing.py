605972d41f6cad205396a2d886134046037dcd30
denis <preductor@gmail.com>
2020-11-20 17:53:31 +0300

    start of training

--- data_processing.py
+++ data_processing.py
@@ -20,17 +20,19 @@ def get_file_root(pth=''):
 def parse_file(path_file):
     root = get_file_root(path_file)
 
-    punctuation_regex = re.compile("[.?!;]")
+    punctuation_regex = re.compile("[\"\'.-?!;]")
+
     words, subpart_of_speechs, genesyss, semantics1s, semantics2s, forms, reducts, upper_cases, vowels, befores, afters, lengths, \
-            PunktBegs, PunktEnds, EmphBegs, EmphEnds = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+            PunktBegs, PunktEnds, EmphBegs, EmphEnds, intonations, pauses = [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
 
     for item in root.findall('./sentence'):
+        num_word = 0
+
         amount = 0
         for idx, child in enumerate(item):
             if child.tag == 'word':
                 amount += 1
         for idx, child in enumerate(item):
-            num_word = 0
             [word, subpart_of_speech, genesys, semantics1, semantics2, form,
                      reduct, upper_case, vowel, before, after, length] = [None]*12
             if child.tag == 'word':
@@ -38,20 +40,24 @@ def parse_file(path_file):
 
                 if 'original' in child.attrib:
                     word = child.attrib['original']
-                    word = word[:-1] if len(punctuation_regex.findall(word[:-1])) > 0 else word
-                    before = num_word = 1
+                    while not (len(word)>0 or word[-1].isalpha()):
+                        word = word[:-1]
+                    while not (len(word)>0 or word[0].isalpha()):
+                        word = word[1:]
+
+                    before = num_word - 1
                     after = amount - num_word
                     length = len(word)
                 for i in child.findall('./dictitem'):
                     if 'subpart_of_speech' in i.attrib:
                         subpart_of_speech = i.attrib['subpart_of_speech']
-                    elif 'genesys' in i.attrib:
+                    if 'genesys' in i.attrib:
                         genesys = i.attrib['genesys']
-                    elif 'semantics1' in i.attrib:
+                    if 'semantics1' in i.attrib:
                         semantics1 = i.attrib['semantics1']
-                    elif 'semantics2' in i.attrib:
+                    if 'semantics2' in i.attrib:
                         semantics2 = i.attrib['semantics2']
-                    elif 'form' in i.attrib:
+                    if 'form' in i.attrib:
                         form = i.attrib['form']
                     break
                 reduct, upper_case, vowel = [], 0, []
@@ -83,6 +89,7 @@ def parse_file(path_file):
 
                 [PunktBeg, PunktEnd, EmphBeg, EmphEnd] = [None]*4
                 if item[idx-1].tag == 'content':
+                    child = item[idx-1]
                     if 'PunktBeg' in child.attrib:
                         PunktBeg = child.attrib['PunktBeg']
                     elif 'PunktEnd' in child.attrib:
@@ -90,104 +97,49 @@ def parse_file(path_file):
                     elif 'EmphBeg' in child.attrib:
                         EmphBeg = child.attrib['EmphBeg']
                     elif 'EmphEnd' in child.attrib:
-                        EmphBeg = child.attrib['EmphEnd']
+                        EmphEnd = child.attrib['EmphEnd']
                 PunktBegs.append(PunktBeg)
                 PunktEnds.append(PunktEnd)
                 EmphBegs.append(EmphBeg)
                 EmphEnds.append(EmphEnd)
 
+                [intonation, pause] = [None] * 2
+                if item[idx + 1].tag == 'intonation':
+                    child = item[idx + 1]
+                    if 'type' in child.attrib:
+                        intonation = child.attrib['type']
+
+                intonations.append(intonation)
+
+                if item[idx + 2].tag == 'pause':
+                    child = item[idx + 2]
+                    if 'type' in child.attrib:
+                        pause = child.attrib['type']
+
+                pauses.append(pause)
 
 
-    df = pd.DataFrame(data = [words, \
+
+
+
+    df = pd.DataFrame(data = [words,
                       subpart_of_speechs, genesyss, semantics1s, semantics2s, forms, reducts, upper_cases, vowels, befores, afters, lengths, \
-            PunktBegs, PunktEnds, EmphBegs, EmphEnds])
+            PunktBegs, PunktEnds, EmphBegs, EmphEnds, intonations, pauses])
 
     df = df.transpose()
     df.columns = ['word', 'subpart_of_speech', 'genesys', 'semantics1', 'semantics2', 'form', 'reduct', 'upper_case',
-                                'vowel', 'before', 'after', 'length', 'PunktBeg', 'PunktEnd', 'EmphBeg', 'EmphEnd']
+                                'vowel', 'before', 'after', 'length', 'PunktBeg', 'PunktEnd', 'EmphBeg', 'EmphEnd', 'intonation', 'pause']
 
-    df.to_csv('./data/features.csv', index=False)
-
-
+    df.to_csv(r'C:\Users\denis\PycharmProjects\TTs_for_STC\data\features.csv', index=False)
 
 
-#                 features_i = []
-#                 mfcc_i = []
-#                 if len(child.findall("./allophone"))>1:
-#                     for letter in child.findall("./allophone"):
-#                         letter_i = letter.attrib
-#                         rm_i.append(letter_i["Rm"])
-#                         en_i.append(letter_i["En"])
-#                         features_i.append(letter_i["features"])
-#                         mfcc_i.append(letter_i["mfcc"])
-#
-#                     vector_i['word'] = word[:-1] if len(punctuation_regex.findall(word[:-1]))>0 else word
-#                     vector_i['Rm'] = rm_i
-#                     vector_i['En'] = en_i
-#                     vector_i["features"] = features_i
-#                     vector_i["mfcc"] = mfcc_i
-#
-#                     corpus_i["word"] = word[:-1] if len(punctuation_regex.findall(word[:-1]))>0 else word
-#
-#                     if len(punctuation_regex.findall(word))>0:
-#                         ispunct = 1
-#                         punct = word[-1]
-#                     else:
-#                         ispunct = 0
-#                         punct = 'a'
-#
-#                     vector_i['ispunctuation'] = ispunct
-#                     vector_i['punct'] = punct
-#                     corpus_i['ispunctuation'] = ispunct
-#                     corpus_i['punct'] = punct
-#                     corpus.append(corpus_i)
-#                     vectors.append(vector_i)
-#     return corpus, vectors
-#
-# def savetoCSV(newsitems, filename, fields):
-#     # specifying the fields for csv file
-#
-#     # writing to csv file
-#     with open(filename, 'w') as csvfile:
-#         # creating a csv dict writer object
-#         writer = csv.DictWriter(csvfile, fieldnames=fields)
-#
-#         # writing headers (field names)
-#         writer.writeheader()
-#
-#         # writing data rows
-#         writer.writerows(newsitems)
+
     # Press the green button in the gutter to run the script.
-def convert_feats_to_tensor(arr, pth):
-    rm = arr
-    rm = [ast.literal_eval(rm_i) for rm_i in rm]
-    rms_padded = []
-    max_rm = max([len(l) for l in rm])
-    for i in rm:
-        pad_len = max_rm - len(i)
-        padded = [int(ii) for ii in i] + [0] * pad_len
-        rms_padded.append(padded)
-    np.save(pth, np.array(rms_padded))
 
 def process(path_file):
     parse_file(path_file)
-    # savetoCSV(corpus, './data/corpus.csv', list(corpus[0].keys()))
-    # savetoCSV(vectors, './data/vectors.csv', list(vectors[0].keys()))
-    #
-    # df2 = pd.read_csv('./data/vectors.csv', encoding='latin-1')
-    # features = []
-    # lens = []
-    # for row in df2["features"].values:
-    #     l = [[re.sub('\D', '', i) for i in feat[1:-1].split("|") if re.sub('\D', '', i)!=''] for feat in row.split(", ")]
-    #     lens.append(len(l))
-    #     features.append(l)
-    # features = np.array(features)
-    # np.save("./features.npy", features)
-    # vectors = pd.read_csv('./data/vectors.csv', encoding='latin-1')
-    # convert_feats_to_tensor(vectors["Rm"], "./rms.npy")
-    #
-    # print(min(lens), max(lens), np.mean(lens))
+
 
 if __name__ == "__main__":
-    path_file = 'data/veshnie-vody.Result.xml'
+    path_file = r'C:\Users\denis\PycharmProjects\TTs_for_STC\data\gieroi_nashiegho_vriemieni.Result.xml'
     process(path_file)
--- main.py
+++ main.py
@@ -19,35 +19,13 @@ from nltk import word_tokenize
 from sklearn.metrics import plot_confusion_matrix
 import matplotlib.pyplot as plt
 
-def anova(model):
-    anova_filter = SelectKBest(f_regression, k=19)
-    anova_svm = make_pipeline(anova_filter, model)
-    return anova_svm
-
-def svm(kernel='rbf', C=10, gamma='scale'):
-    svc = SVC(kernel=kernel, class_weight='balanced', C=C, degree=5, tol=1e-5, gamma=gamma)
-    return svc
-
-def linear_svm():
-    svc = LinearSVC(C = 1, random_state=0, tol=1e-5, dual=False, fit_intercept=True, class_weight='balanced')
-    return svc
-
-def voting(models):
-    l = []
-    for i, m in enumerate(models):
-        l.append(("clf_{}".format(i), m))
-    eclf = VotingClassifier(estimators = l,
-                            voting = 'hard')
-    return eclf
-
-def adaboost():
-    adaboost = AdaBoostClassifier(
-                DecisionTreeClassifier(max_depth=50),
+def rf():
+    classifier = RandomForestClassifier(max_depth=50,
                 n_estimators=200, random_state=0)
-    return adaboost
+    return classifier
 
 def train(model, data, y):
-    kf = KFold(n_splits=2, random_state=None)
+    kf = KFold(n_splits=3, random_state=None)
     scores = []
     for train_index, val_index in kf.split(data):
         X_train, X_val = data[train_index], data[val_index]
@@ -57,7 +35,7 @@ def train(model, data, y):
     print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
     return model, scores
 
-def test(model, x_test, y):
+def test_model(model, x_test, y):
     preds = model.predict(x_test)
     score1 = f1_score(y, preds, average='micro')
     score2 = f1_score(y, preds, average='macro')
@@ -67,54 +45,26 @@ def test(model, x_test, y):
           " [accuracy: %0.3f], [roc_auc: %0.3f])" % (score1, score2, accuracy, roc_auc))
     return preds, score1, score2
 
-def get_pre_learned():
-    i = 0
-    embs = []
-    words = []
-    with open(r'C:\Users\denis\Documents\180\model.txt', 'r', encoding='utf-8') as f:
-        text = f.readlines()
-        for row in text:
-            data = row.split(' ')
-            word = data[0].split('_')[0]
-
-            emb = [float(i) for i in data[1:]]
-            words.append(word)
-            embs.append(emb)
-    return words, embs
-
-
-def get_fasttext(df, pre_words, pre_embs):
-    embs = []
-
-    words = np.array(df['word'].values).reshape(df.shape[0], 1)
-
-    model = fasttext.FastText(words, size=100, window=2, min_count=0)
-    for word in tqdm(df["word"].values, total=df.shape[0]):
-        if word in pre_words:
-            i = pre_words.index(word)
-            emb = pre_embs[i]
-        else:
-            emb = model.wv[word]
-        embs.append(emb)
-    embs = np.array(embs)
-    np.save('./fasttext.npy', embs)
-    return embs
-
 
 if __name__ == '__main__':
-    corpus = pd.read_csv('./data/corpus.csv')
-    vectors = pd.read_csv('./data/vectors.csv')
-    y = corpus['ispunctuation'].values
-    features = np.load("./features.npy", allow_pickle=True)
-    feats = np.load('./word2vec.npy')
-    rms = np.load('./rms.npy')
-    X_train, X_test, y_train, y_test = train_test_split(rms, y, test_size=0.1, random_state=42)
+    features_path = r'C:\Users\denis\PycharmProjects\TTs_for_STC\data\features.csv'
+    df = pd.read_csv(features_path)
+    df.fillna(-1)
+
+    # first task
+    y1 = df['pause'] != -1
+    y1 = [int(x) for x in y1]
+    X_train, X_test, y_train, y_test = train_test_split(df.drop(columns=['intonation', 'pause', 'vowel', 'reduct']), y1, test_size=0.1, random_state=42)
     # model = svm(kernel='rbf')
     # model2 = svm(kernel='rbf', C=1, gamma='auto')
     # model3 = svm(kernel='poly')
     # model4 = voting([model, model2, model3])
-    adaboost_m = adaboost()
-    model, scores = train(adaboost_m, X_train, y_train)
-    preds, score1, score2 = test(adaboost_m, X_test, y_test)
-    print(adaboost_m.get_params())
+    rf_model = rf()
+    model, scores = train(rf_model, X_train, y_train)
+    preds, score1, score2 = test_model(model, X_test, y_test)
+    print(scores)
+    print(score1)
+    print(score2)
+    print(model.get_params())
     #get_word2vec(vectors)
+
